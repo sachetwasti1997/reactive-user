ffaacb9df060ce3a8773f2530c77f65a7eb44e99
sachetwasti1997 <sachetwasti61@gmail.com>
2021-07-24 18:41:05 +0530

    [UPDATE] Added Code to get userTransaction according to userId

--- src/main/java/com/sachet/userservice/controller/UserTransactionController.java
+++ src/main/java/com/sachet/userservice/controller/UserTransactionController.java
@@ -2,11 +2,10 @@ package com.sachet.userservice.controller;
 
 import com.sachet.userservice.dto.TransactionRequestDto;
 import com.sachet.userservice.dto.TransactionResponseDto;
+import com.sachet.userservice.entity.UserTransaction;
 import com.sachet.userservice.service.ReactiveTransactionService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -22,6 +21,11 @@ public class UserTransactionController {
     @PostMapping
     public Mono<TransactionResponseDto> createTransaction(@RequestBody Mono<TransactionRequestDto> transactionRequest){
         return transactionRequest.flatMap(reactiveTransactionService::createTransaction);
+    }
+
+    @GetMapping("{userId}")
+    public Flux<UserTransaction> getTransactionByUserId(@PathVariable Integer userId){
+        return reactiveTransactionService.getTransactionByUserId(userId);
     }
 
 
--- src/main/java/com/sachet/userservice/service/ReactiveTransactionService.java
+++ src/main/java/com/sachet/userservice/service/ReactiveTransactionService.java
@@ -3,10 +3,12 @@ package com.sachet.userservice.service;
 import com.sachet.userservice.dto.TransactionRequestDto;
 import com.sachet.userservice.dto.TransactionResponseDto;
 import com.sachet.userservice.dto.TransactionStatus;
+import com.sachet.userservice.entity.UserTransaction;
 import com.sachet.userservice.repository.UserRepository;
 import com.sachet.userservice.repository.UserTransactionRepository;
 import com.sachet.userservice.util.EntityDtoUtil;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -28,6 +30,10 @@ public class ReactiveTransactionService {
                 .flatMap(userTransactionRepository::save)
                 .map(userTransaction -> EntityDtoUtil.toResponseDto(transactionRequestDto, TransactionStatus.APPROVED))
                 .defaultIfEmpty(EntityDtoUtil.toResponseDto(transactionRequestDto, TransactionStatus.REJECTED));
+    }
+
+    public Flux<UserTransaction> getTransactionByUserId(Integer userId){
+        return userTransactionRepository.findByUserId(userId);
     }
 
 
